60a45c663b68af2b8950ddcb24c1d0b98e930c35
gd-matsui-sgc <147043990+gd-matsui-sgc@users.noreply.github.com>
2024-10-12 15:34:01 +0900

    テスト環境を追加

--- Unity/GameCamp2024/Assets/Scripts/Main.cs
+++ Unity/GameCamp2024/Assets/Scripts/Main.cs
@@ -6,43 +6,62 @@ using UnityEngine.SceneManagement;
 
 public class Main : BaseScene
 {
+    // フェイズ
     public enum Phase
     {
         Logo,
         Title,
         Game,
         Result,
+        Test_Ogasawara,
+        Test_Nohara,
+        Test_Matsui,       
     };
 
-    private Logo   m_logo = null;
+    // シーン関連
+    private Logo   m_logo       = null;     // ロゴ
+    private Title   m_title     = null;     // タイトル
+    private Game    m_game      = null;     // ゲーム
+    private Result  m_result    = null;     // リザルト
+    private Test_Ogasawara  m_testOgasawara = null;
+    private Test_Nohara     m_testNohara = null;
+    private Test_Matsui     m_testMatsui = null;
 
-    private Title   m_title = null;
-    private Game    m_game = null;
-    private Result  m_result = null;
+    [SerializeField]
+    private Phase m_startupPhase = Phase.Logo;  // 開始フェーズ
 
-
-
-
-
-    // Start is called before the first frame update
+    /**
+     * Updateの直前に呼ばれる
+     */
     protected override void OnStart()
     {
-        SetPhase((int)Phase.Game);
+        SetPhase((int)m_startupPhase);
     }
 
-    // Update is called once per frame
+    /*
+     * 毎フレーム呼ばれる
+     */
     protected override void OnUpdate()
     {
-        Debug.Log(" " + GetPhaseTime());
+        if(GetPhaseTime() == 0 )
+        {
+            Debug.Log("CurrenPhase = " + (Phase)GetPhase());
+        }
         switch((Phase)GetPhase())
         {
         case Phase.Logo:    _Logo();    break;
         case Phase.Title:   _Title();   break;
         case Phase.Game:    _Game();    break;
         case Phase.Result:  _Result();  break;
+        case Phase.Test_Ogasawara:  _TestOgasawara();  break;
+        case Phase.Test_Nohara:     _TestNohara();  break;
+        case Phase.Test_Matsui:     _TestMatsui();  break;
         }
     }
 
+    /**
+     * ロゴ
+     */
     private void _Logo()
     {
         if( GetPhaseTime() == 0 )
@@ -60,7 +79,9 @@ public class Main : BaseScene
         }
     }
 
-
+    /**
+     * タイトル
+     */
     private void _Title()
     {
         if( GetPhaseTime() == 0 )
@@ -78,6 +99,9 @@ public class Main : BaseScene
         }
     }
 
+    /**
+     * ゲーム
+     */
     private void _Game()
     {
         if( GetPhaseTime() == 0 )
@@ -95,6 +119,9 @@ public class Main : BaseScene
         }
     }
 
+    /**
+     * リザルト
+     */
     private void _Result()
     {
         if( GetPhaseTime() == 0 )
@@ -111,6 +138,67 @@ public class Main : BaseScene
             }
         }
     }
+
+    /**
+     * 小笠原さん用テスト
+     */
+    private void _TestOgasawara()
+    {
+        if( GetPhaseTime() == 0 )
+        {
+            StartCoroutine("CreateTestOgasawara");
+        }
+        else if( m_testOgasawara != null)
+        {
+            if( m_testOgasawara.IsExited())
+            {
+                m_testOgasawara = null;
+                SceneManager.UnloadSceneAsync("Test_Ogasawara");
+                SetPhase(  ( int )Phase.Title );
+            }
+        }
+    }
+
+    /**
+     * 野原さん用テスト
+     */
+    private void _TestNohara()
+    {
+        if( GetPhaseTime() == 0 )
+        {
+            StartCoroutine("CreateTestNohara");
+        }
+        else if( m_testNohara != null)
+        {
+            if( m_testNohara.IsExited())
+            {
+                m_testNohara = null;
+                SceneManager.UnloadSceneAsync("Test_Nohara");
+                SetPhase(  ( int )Phase.Title );
+            }
+        }
+    }
+
+    /**
+     * 松井用テスト
+     */
+    private void _TestMatsui()
+    {
+        if( GetPhaseTime() == 0 )
+        {
+            StartCoroutine("CreateTestMatsui");
+        }
+        else if( m_testMatsui != null)
+        {
+            if( m_testMatsui.IsExited())
+            {
+                m_testMatsui = null;
+                SceneManager.UnloadSceneAsync("Test_Matsui");
+                SetPhase(  ( int )Phase.Title );
+            }
+        }
+    }
+
 
     /**
      * ロゴ生成のコルーチン
@@ -178,6 +266,54 @@ public class Main : BaseScene
 
         GameObject result = GameObject.Find("Result");
         m_result = result?.GetComponent<Result>();
+    }
+
+    /**
+     * 小笠原さん用テスト生成のコルーチン
+     */
+    IEnumerator CreateTestOgasawara()
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync( "Test_Ogasawara", LoadSceneMode.Additive );
+        yield return null;
+        while(!operation.isDone)
+        {
+            yield return null;
+        }
+
+        GameObject testOgasawara = GameObject.Find("Test_Ogasawara");
+        m_testOgasawara = testOgasawara?.GetComponent<Test_Ogasawara>();
+    }
+
+    /**
+     * 野原さん用テスト生成のコルーチン
+     */
+    IEnumerator CreateTestNohara()
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync( "Test_Nohara", LoadSceneMode.Additive );
+        yield return null;
+        while(!operation.isDone)
+        {
+            yield return null;
+        }
+
+        GameObject testNohara = GameObject.Find("Test_Nohara");
+        m_testNohara = testNohara?.GetComponent<Test_Nohara>();
+    }
+
+    /**
+     * 松井用テスト生成のコルーチン
+     */
+    IEnumerator CreateTestMatsui()
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync( "Test_Matsui", LoadSceneMode.Additive );
+        yield return null;
+        while(!operation.isDone)
+        {
+            yield return null;
+        }
+
+        GameObject testMatsui = GameObject.Find("Test_Matsui");
+        m_testMatsui = testMatsui?.GetComponent<Test_Matsui>();
     }
 
 }
---
+++ Unity/GameCamp2024/Assets/Scripts/Test_Ogasawara.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_Ogasawara : Base
+{
+    // Start is called before the first frame update
+    protected override void OnStart()
+    {
+        
+    }
+
+    // Update is called once per frame
+    protected override void OnUpdate()
+    {
+        if(GetPhaseTime() == 100)
+        {
+            Exit();
+        }
+    }
+}

b8c781a877cc578d559ff5aea65c52c97782cc72
gd-matsui-sgc <147043990+gd-matsui-sgc@users.noreply.github.com>
2024-10-13 02:33:36 +0900

    テロップのクラス追加（中身はまだ）

---
+++ Unity/GameCamp2024/Assets/Scripts/Telop.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; //UI機能を扱うときに追記する
+using TMPro;
+
+/*
+ * テロップ用クラス
+ */
+public class Telop : Base
+{
+	// フェーズ
+	public enum Phase
+	{
+		// 待機
+		Idle,
+
+		// 再生
+		Play,
+	}
+
+    /**
+     * Updateの直前に呼ばれる
+     */
+	protected override void OnStart()
+    {
+
+    }
+
+    /*
+     * 毎フレーム呼ばれる
+     */
+	protected override void OnUpdate()
+    {
+		switch((Phase)GetPhase())
+		{
+		case Phase.Idle:				break;
+		case Phase.Play:	_Play();	break;
+		}
+	}
+
+    /*
+     * 再生フェイズ
+     */
+    private void _Play()
+    {
+
+    }
+
+}
